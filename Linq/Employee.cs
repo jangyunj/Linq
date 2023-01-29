c6c548b1a9ac5a8b641cba0b4f92b574bc3f1393
Yun Jang <jang.yunj7@gmail.com>
2023-01-28 20:12:36 -0500

    initial commit

---
+++ Linq/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Employee
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+        public int YearsOfExperience { get; set; }
+        public Employee(string aFirstName, string aLastName, int aAge, int aYearsOfExperience)
+        {
+            FirstName = aFirstName;
+            LastName = aLastName;
+            Age = aAge;
+            YearsOfExperience = aYearsOfExperience;
+        }
+
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+Console.WriteLine("Sum: ");
+Console.WriteLine(numbers.Sum());
+
+Console.WriteLine("Average: ");
+Console.WriteLine(numbers.Average());
+
+Console.WriteLine("Numbers in Ascending order: ");
+numbers.OrderBy(x => x).ToList().ForEach(x => Console.WriteLine(x));
+
+Console.WriteLine("Numbers in Descending order: ");
+numbers.OrderByDescending(x => x).ToList().ForEach(x => Console.WriteLine(x));
+
+Console.WriteLine("Numbers greater than 6: ");
+numbers.Where(x => x > 6).ToList().ForEach(x => Console.WriteLine(x));
+
+Console.WriteLine("Numbers in Ascending order but only 4 of them: ");
+var ordered = numbers.OrderBy(x => x);
+foreach (var item in ordered.Take(4))
+{
+    Console.WriteLine(item);
+}
+
+//OR
+//numbers.OrderBy(x => x).Take(4).ToList().ForEach(x => Console.WriteLine(x));
+
+Console.WriteLine("Change the value at index 4 to 100, then print the numbers in ascending order: ");
+numbers.SetValue(100, 4);
+//OR numbers[4] = 100;
+numbers.OrderBy(x => x).ToList().ForEach(x => Console.WriteLine(x));
