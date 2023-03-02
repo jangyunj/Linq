c72e918e9af213c9a3d7c6b4cae8a303704db710
Yun Jang <jang.yunj7@gmail.com>
2023-03-01 23:48:06 -0500

    added new prob

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -75,3 +75,15 @@ static List<Employee> CreateEmployees()
 }
 
 #endregion
+
+//Write Linq for: The numbers which produce the remainder 0 after divided by 2 
+int[] n1 = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+var nQuery =from VrNum in n1
+            where (VrNum % 2) == 0
+            select VrNum;
+
+foreach (int VrNum in nQuery)
+{
+    Console.Write("{0} ", VrNum);
+}
